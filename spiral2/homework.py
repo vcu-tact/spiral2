76e5e7f7a63865167aaad434c15ff697df59eaf4
yourusername <tact@vcu.edu>
2021-04-30 22:00:48 -0400

    added in code for homework

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,24 @@
 def spiralize(size, n=1):
-    """ Your code goes somewhere in here"""
-    return_value = n
-    return return_value
+    dSum = n
+    currentNum = n
+    blocks = size * size + n - 1 
+    counter = 0
+    inc = 2
+
+
+    while currentNum < blocks:
+        currentNum = currentNum + inc
+        dSum = dSum + currentNum
+        counter+=1
+    
+        if counter % 4 == 0:
+            inc = inc + 2
+    return dSum
+
+def main():
+    test = spiralize (11,27)
+    print(test)
+    # just testing some code here 
+
+if __name__ == "__main__":
+    main()
