1be19e7bf3738090fae9ff6120a723ac366dd247
yourusername <tact@vcu.edu>
2021-04-30 22:18:45 -0400

    reformatted with black

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,24 +1,25 @@
 def spiralize(size, n=1):
     dSum = n
     currentNum = n
-    blocks = size * size + n - 1 
+    blocks = size * size + n - 1
     counter = 0
     inc = 2
-
 
     while currentNum < blocks:
         currentNum = currentNum + inc
         dSum = dSum + currentNum
-        counter+=1
-    
+        counter += 1
+
         if counter % 4 == 0:
             inc = inc + 2
     return dSum
 
+
 def main():
-    test = spiralize (11,27)
+    test = spiralize(11, 27)
     print(test)
-    # just testing some code here 
+    # just testing some code here
+
 
 if __name__ == "__main__":
     main()
